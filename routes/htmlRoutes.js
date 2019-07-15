144d5891a442dd04e67de8892eb3cfbec59440bc
Jyoti Das <bootcampatut@gmail.com>
2019-07-15 15:28:51 -0500

    initial login page and authrization done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,24 @@
 var db = require("../models");
+// =============================================================
+var path = require("path");
+var passport=require("../config/passport");
 
+//loads the login page
 module.exports = function(app) {
+ 
+  app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+   });
+
+  app.get("/login",function(req,res){
+  res.sendFile(path.join(__dirname, "../public/login.html"));
+ });
+//loads the register page
+ app.get("/register",function(req,res){
+  res.sendFile(path.join(__dirname, "../public/register.html"));
+ });
+ 
+ 
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
