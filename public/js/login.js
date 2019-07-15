ad6c729734c57e22e8e52ced4180b0af2df93d07
Jyoti Das <bootcampatut@gmail.com>
2019-07-15 16:07:01 -0500

    Linked the register page to the home page

--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,54 @@
 $(document).ready(function () {
       console.log("Inside the js");
-      
+
       //Eventlistner for register submit
       $(document).on("click", "#register_submit", function (event) {
             console.log("inside the click");
             event.preventDefault();
             var postregister = {
-                  firstname:$("#cmsInputName").val().trim(),
+                  firstname: $("#cmsInputName").val().trim(),
                   lastname: $("#cmsInputLastName").val().trim(),
-                  email:$("#cmsInputEmail1").val().trim(),
-                  password:$("#cmsInputPassword1").val().trim(),
-                  cnfpassword:$("#cmsConfirmPassword").val().trim()
+                  email: $("#cmsInputEmail1").val().trim(),
+                  password: $("#cmsInputPassword1").val().trim(),
+                  cnfpassword: $("#cmsConfirmPassword").val().trim()
             };
             console.log(postregister);
             $.ajax("/api/register", {
                   type: "POST",
                   data: postregister
-            }).then(function () {
+            }).then(function (data) {
                   console.log("Added new Record");
+                  alert("data added");
                   // Reload the page to get the updated list
                   location.reload();
+
             });
       });
-// Event listner for login check 
+      // Event listner for login check 
       $(document).on("click", "#login", function (event) {
             console.log("inside the click");
             event.preventDefault();
-            var login={
-                  email:$("#inputEmail").val().trim(),
-                  password:$("#inputPassword").val().trim()
+            var login = {
+                  email: $("#inputEmail").val().trim(),
+                  password: $("#inputPassword").val().trim()
             };
             console.log(login);
-            $.ajax("/api/login",{
-                  type:"POST",
-                  data:login
-            }).then(function(data){
-                  if(data.message===false){
-                        var  p=$("<p>");
+            $.ajax("/api/login", {
+                  type: "POST",
+                  data: login
+            }).then(function (data) {
+                  if (data.message === false) {
+                        var p = $("<p>");
                         p.addClass("errortag");
                         p.text("User details does not match");
                         p.css("color", "red");
                         $("#error").append(p);
                         console.log("no match");
-                  }
-                  else{
-                        $.ajax("/api/welcome",{
-                              type:"GET",
-                              data:data
-                        }).then (function(data){
+                  } else {
+                        $.ajax("/api/welcome", {
+                              type: "GET",
+                              data: data
+                        }).then(function (data) {
                               console.log("sucessfully landed on welcome page");
                         });
                   }
