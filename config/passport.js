846f6fe056f90287f478bdab5d07f12e45559313
Jyoti Das <bootcampatut@gmail.com>
2019-07-16 12:32:06 -0500

    fixed spacing

--- config/passport.js
+++ config/passport.js
@@ -3,33 +3,33 @@ var LocalStrategy = require("passport-local").Strategy;
 var db = require("../models");
 
 passport.use(
-      new LocalStrategy({
-                  usernameField: "email"
-            },
-            function (email, password, done) {
-                  db.User.findOne({
-                        where: {
-                              email:email
-                        }
-                  }).then(function (dbUser) {
-                        if (!dbUser) {
-                              return done(null, false, {
-                                    message: "incorrect user"
-                              });
-                        } else if (!dbUser.validPassword(password)) {
-                              return done(null, false, {
-                                    message: "incorrect Password"
-                              });
-                        }
-                        return done(null, dbUser);
-                  });
-            }
-      )
+  new LocalStrategy({
+    usernameField: "email"
+  },
+  function (email, password, done) {
+    db.User.findOne({
+      where: {
+        email:email
+      }
+    }).then(function (dbUser) {
+      if (!dbUser) {
+        return done(null, false, {
+          message: "incorrect user"
+        });
+      } else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "incorrect Password"
+        });
+      }
+      return done(null, dbUser);
+    });
+  }
+  )
 );
 passport.serializeUser(function (user, cb) {
-      cb(null, user);
+  cb(null, user);
 });
 passport.deserializeUser(function (obj, cb) {
-      cb(null, obj);
+  cb(null, obj);
 });
 module.exports = passport;
--- public/js/login.js
+++ public/js/login.js
@@ -1,64 +1,64 @@
 $(document).ready(function () {
-      console.log("Inside the js");
+  console.log("Inside the js");
       
-      //Eventlistner for register submit
-      $(document).on("click", "#register_submit", function (event) {
-            console.log("inside the click");
-            event.preventDefault();
-            var postregister = {
-                  firstname:$("#cmsInputName").val().trim(),
-                  lastname: $("#cmsInputLastName").val().trim(),
-                  email:$("#cmsInputEmail1").val().trim(),
-                  password:$("#cmsInputPassword1").val().trim(),
-                  cnfpassword:$("#cmsConfirmPassword").val().trim()
-            };
-            console.log(postregister);
-            $.ajax("/api/register", {
-                  type: "POST",
-                  data: postregister
-            }).then(function () {
-                  console.log("Added new Record");
-                  // Reload the page to get the updated list
-                  location.reload();
-            });
-      });
-// Event listner for login check 
-      $(document).on("click", "#login", function (event) {
-            console.log("inside the click");
-            event.preventDefault();
-            var login={
-                  email:$("#inputEmail").val().trim(),
-                  password:$("#inputPassword").val().trim()
-            };
-            console.log(login);
-            $.ajax("/api/login",{
-                  type:"POST",
-                  data:login
-            }).then(function(data){
-                  if(data.message===false){
-                        var  p=$("<p>");
-                        p.addClass("errortag");
-                        p.text("User details does not match");
-                        p.css("color", "red");
-                        $("#error").append(p);
-                        console.log("no match");
-                  }
-                  else{
-                        $.ajax("/api/welcome",{
-                              type:"GET",
-                              data:data
-                        }).then (function(data){
-                              console.log("sucessfully landed on welcome page");
-                        });
-                  }
-            });
+  //Eventlistner for register submit
+  $(document).on("click", "#register_submit", function (event) {
+    console.log("inside the click");
+    event.preventDefault();
+    var postregister = {
+      firstname:$("#cmsInputName").val().trim(),
+      lastname: $("#cmsInputLastName").val().trim(),
+      email:$("#cmsInputEmail1").val().trim(),
+      password:$("#cmsInputPassword1").val().trim(),
+      cnfpassword:$("#cmsConfirmPassword").val().trim()
+    };
+    console.log(postregister);
+    $.ajax("/api/register", {
+      type: "POST",
+      data: postregister
+    }).then(function () {
+      console.log("Added new Record");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+  // Event listner for login check 
+  $(document).on("click", "#login", function (event) {
+    console.log("inside the click");
+    event.preventDefault();
+    var login={
+      email:$("#inputEmail").val().trim(),
+      password:$("#inputPassword").val().trim()
+    };
+    console.log(login);
+    $.ajax("/api/login",{
+      type:"POST",
+      data:login
+    }).then(function(data){
+      if(data.message===false){
+        var p=$("<p>");
+        p.addClass("errortag");
+        p.text("User details does not match");
+        p.css("color", "red");
+        $("#error").append(p);
+        console.log("no match");
+      }
+      else{
+        $.ajax("/api/welcome",{
+          type:"GET",
+          data:data
+        }).then (function(data){
+          console.log("sucessfully landed on welcome page");
+        });
+      }
+    });
 
-      });
-
-
+  });
 
 
 
 
-      //End of document.ready function()     
+
+
+  //End of document.ready function()     
 });
