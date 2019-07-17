cdd754a88a62006e552bc55e1dfb0a07bec36467
Jyoti Das <bootcampatut@gmail.com>
2019-07-17 11:34:56 -0500

    spaces on book

--- public/js/book.js
+++ public/js/book.js
@@ -22,38 +22,38 @@ $(document).ready(function () {
 
                  
 
-//   function searchbyAuthor(category, author) {
-//     var apikey = process.env.api_key;
-//     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&inauthor=" + author + "&api_key=" + apikey;
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function (response) {
-//       console.log(response);
-//     });
-//   }
+  //   function searchbyAuthor(category, author) {
+  //     var apikey = process.env.api_key;
+  //     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&inauthor=" + author + "&api_key=" + apikey;
+  //     $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     }).then(function (response) {
+  //       console.log(response);
+  //     });
+  //   }
 
-//   function searchbyPublisher(category, publisher) {
-//     var apikey = process.env.api_key;
-//     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&inpublisher=" + publisher + "&api_key=" + apikey;
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function (response) {
-//       console.log(response);
-//     });
-//   }
+  //   function searchbyPublisher(category, publisher) {
+  //     var apikey = process.env.api_key;
+  //     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&inpublisher=" + publisher + "&api_key=" + apikey;
+  //     $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     }).then(function (response) {
+  //       console.log(response);
+  //     });
+  //   }
 
-//   function searchbytitle(category, title) {
-//     var apikey = process.env.api_key;
-//     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&intitle=" + title + "&api_key=" + apikey;
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function (response) {
-//       console.log(response);
-//     });
-//   }
+  //   function searchbytitle(category, title) {
+  //     var apikey = process.env.api_key;
+  //     var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + category + "&intitle=" + title + "&api_key=" + apikey;
+  //     $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     }).then(function (response) {
+  //       console.log(response);
+  //     });
+  //   }
 });
 
 
