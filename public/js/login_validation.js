817067c03bd5d02902f5a7d268509e3a13b77e48
Nikita Mehata <nikitamehata23@gmail.com>
2019-07-18 14:13:52 -0500

    working on login_validation.js

--- public/js/login_validation.js
+++ public/js/login_validation.js
@@ -1,22 +1,59 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "asdf" && password == "asdf#123"){
-alert ("Login successfully");
-window.location = "admin.html"; // Redirecting to other page.
-return false;
-}
-else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
+var form = document.createElement('form');
+if ('checkValidity' in form && 'querySelector' in document && 'classList' in document.documentElement) {
+
+    // get all inputs
+    var inputs = document.querySelectorAll("[data-error]");
+
+    if (inputs.length > 0) {
+
+        var toggleErrorMessage = function(input, hasError) {
+            var message = (input.value === '') ? input.dataset.empty : input.dataset.error;
+            var oldMessage = document.getElementById("alert-" + input.name);
+            var newMessage;
+
+            if (hasError) {
+                if (!oldMessage) {
+                    newMessage = document.createElement("p");
+                    newMessage.setAttribute('role', 'alert');
+                    newMessage.classList.add('form__error');
+                    newMessage.setAttribute('id', 'alert-' + input.name);
+                } else {
+                    newMessage = oldMessage;
+                }
+
+                newMessage.innerText = message;
+
+                input.setAttribute('aria-describedby', 'alert-' + input.name);
+                input.parentElement.appendChild(newMessage);
+
+                input.parentElement.classList.add('has-error');
+            } else {
+
+                if (oldMessage) {
+                    input.parentElement.removeChild(oldMessage);
+                    input.removeAttribute('aria-describedby');
+                    input.parentElement.classList.remove('has-error');
+                }
+
+            }
+        };
+        // loop over each input
+        [].forEach.call(inputs, function(input) {
+
+            // check validation on blur
+            input.addEventListener("blur", function(event) {
+                input.checkValidity();
+
+                if (input.checkValidity()) {
+                    input.classList.remove("error");
+                    input.setAttribute("aria-invalid", "false");
+                    toggleErrorMessage(input, false);
+                } else {
+                    input.classList.add("error");
+                    input.setAttribute("aria-invalid", "true");
+                    toggleErrorMessage(input, true);
+                }
+            });
+        });
+    }
 }
