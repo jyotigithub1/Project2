b34b666aef37a4d23dd36bcefa95ba36f44ed57a
Joel Hernandez <joel24hernan@gmail.com>
2019-07-18 13:10:11 -0500

    updates book.js to display the books from database. Added css to the books to display.

--- public/js/book.js
+++ public/js/book.js
@@ -1,3 +1,5 @@
+$(".hide-row").hide();
+
 $(document).ready(function () {
   console.log("inside book.js");
   $(document).on("click", "#book-search-btn", function (event) {
@@ -21,40 +23,44 @@ $(document).ready(function () {
         data: { category: category }
       }).then(function (category) {
         console.log(category);
-        for (var i = 0; i < category.length; i++) {
-          console.log(category[i]);
-  
-          //display books and images
-          var bookDiv = $("<div>");
-          bookDiv.addClass("book-div")
-          var bookRow = $("<div>");
-          bookRow.addClass("info-row");
-          var bookTitle = category[i].title;
-          var author = category[i].authors;
-          var publishedDate = category[i].publishedDate;
-          var bookImage = category[i].thumbnail;
-          console.log(bookTitle, author, publishedDate);
-          console.log(bookImage)
-  
-          var p = $("<p>");
-          var titleDiv = $("<div>")
-          var infoDiv = $("<div>")
-          titleDiv.addClass("book-title")
-          titleDiv.html(bookTitle + "<br>")
-          infoDiv.addClass("info-text")
-          infoDiv.html(author + "<br>" + publishedDate + "<br>")
-          var imageDiv = $("<img>");
-          imageDiv.attr("src", bookImage);
-          imageDiv.addClass("images col-6")
-          bookRow.append(imageDiv);
-  
-          p.prepend(titleDiv);
-          p.append(infoDiv);
-          p.addClass("text col-6")
-          bookRow.append(p);
-          bookDiv.append(bookRow);
-          $("#books-area").append(bookDiv);
-        }  //closing of main for loop
+        function displayBooks(){
+          $(".hide-row").show();
+          for (var i = 0; i < category.length; i++) {
+            console.log(category[i]);
+        
+            //display books and images
+            var bookDiv = $("<div>");
+            bookDiv.addClass("book-div")
+            var bookRow = $("<div>");
+            bookRow.addClass("info-row");
+            var bookTitle = category[i].title;
+            var author = category[i].authors;
+            var publishedDate = category[i].publishedDate;
+            var bookImage = category[i].thumbnail;
+            console.log(bookTitle, author, publishedDate);
+            console.log(bookImage)
+        
+            var p = $("<p>");
+            var titleDiv = $("<div>")
+            var infoDiv = $("<div>")
+            titleDiv.addClass("book-title")
+            titleDiv.html(bookTitle + "<br>")
+            infoDiv.addClass("info-text")
+            infoDiv.html(author + "<br>" + publishedDate + "<br>")
+            var imageDiv = $("<img>");
+            imageDiv.attr("src", bookImage);
+            imageDiv.addClass("images col-6")
+            bookRow.append(imageDiv);
+        
+            p.append(titleDiv);
+            p.append(infoDiv);
+            p.addClass("text col-6")
+            bookRow.append(p);
+            bookDiv.append(bookRow);
+            $("#books-area").append(bookDiv);
+            }  //closing of main for loop
+          }
+        displayBooks();
       });
     }
     // elseif(category==="Languages" &&  searchby==="Select"){
